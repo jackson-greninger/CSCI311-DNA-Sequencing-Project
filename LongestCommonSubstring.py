34d9077117996988322b63c7136886a7e44e817e
JacksonGreninger <jwg019@bucknell.edu>
2026-03-24 16:47:10 -0400

    adding cleanfile function

--- LongestCommonSubstring.py
+++ LongestCommonSubstring.py
@@ -1,8 +1,29 @@
-def LCS(input_sequence, database):
+def clean_file(database):
+    sequences = []
+    current_seq = []
+    
+    file = open(database, "r")
+
+    for line in file:
+        line = line.strip()
+            
+        if line.startswith(">"):
+            if current_seq:
+                sequences.append("".join(current_seq))
+                current_seq = []
+        else:
+            current_seq.append(line)
+        
+    return sequences
+
+    if current_seq:
+        sequences.append("".join(current_seq))
+
+
 
 
 if __name__ == "__main__":
     # call LCS function
-    pass
-
-main()
+    target = "CSCI311-DNA-Sequencing-Project\DNA_query.txt"
+    database = "CSCI311-DNA-Sequencing-Project\DNA_sequences.txt"
+    clean_file(database)
